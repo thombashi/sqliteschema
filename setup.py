59608f884d279007728bae0314771f0b9e94e57c
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-07-02 14:45:44 +0900

    Bump version to 0.9.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 
 setuptools.setup(
     name="sqliteschema",
-    version="0.9.1",
+    version="0.9.2",
     author="Tsuyoshi Hombashi",
     author_email="tsuyoshi.hombashi@gmail.com",
     url="https://github.com/thombashi/sqliteschema",
