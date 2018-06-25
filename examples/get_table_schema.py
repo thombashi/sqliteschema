fbfe79563ca1d57c1001791f6c2d7990b7de7da1
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-06-25 22:31:11 +0900

    Update examples

--- examples/get_table_schema.py
+++ examples/get_table_schema.py
@@ -7,6 +7,7 @@
 
 from __future__ import print_function, unicode_literals
 
+import json
 import sys
 
 import simplesqlite
@@ -42,19 +43,19 @@ def make_database():
     return sqlite_db_path
 
 
-def main():
-    sqlite_db_path = make_database()
-    extractor = sqliteschema.SQLiteSchemaExtractor(sqlite_db_path)
+def dump_schema_as_dict(extractor):
+    print("--- dump all of the table schemas with a dictionary ---\n{}\n".format(
+        json.dumps(extractor.fetch_database_schema_as_dict(), indent=4)))
 
+    print("--- dump a specific table schema with a dictionary ---\n{}\n".format(
+        json.dumps(extractor.fetch_table_schema("sampletable1").as_dict(), indent=4)))
+
+
+def dump_schema_as_table(extractor):
     for verbosity_level in range(2):
         print("--- dump all of the table schemas with a tabular format: verbosity_level={} ---".format(
             verbosity_level))
         print(extractor.dumps(output_format="markdown", verbosity_level=verbosity_level))
-
-    for verbosity_level in range(5):
-        print("--- dump all of the table schemas with text format: verbosity_level={} ---".format(
-            verbosity_level))
-        print(extractor.dumps(output_format="text", verbosity_level=verbosity_level) + "\n")
 
     for verbosity_level in range(2):
         print("--- dump a specific table schema with a tabular format: verbosity_level={} ---".format(
@@ -62,11 +63,29 @@ def main():
         print(extractor.fetch_table_schema("sampletable1").dumps(
             output_format="markdown", verbosity_level=verbosity_level))
 
+
+def dump_schema_as_text(extractor):
+    for verbosity_level in range(5):
+        print("--- dump all of the table schemas with text format: verbosity_level={} ---".format(
+            verbosity_level))
+        print(extractor.dumps(output_format="text", verbosity_level=verbosity_level) + "\n")
+
     for verbosity_level in range(5):
         print("--- dump specific table schema with text format: verbosity_level={} ---".format(
             verbosity_level))
         print(extractor.fetch_table_schema("sampletable1").dumps(
             output_format="text", verbosity_level=verbosity_level) + "\n")
+
+
+def main():
+    sqlite_db_path = make_database()
+    extractor = sqliteschema.SQLiteSchemaExtractor(sqlite_db_path)
+
+    dump_schema_as_dict(extractor)
+    print("========================================\n")
+    dump_schema_as_table(extractor)
+    print("========================================\n")
+    dump_schema_as_text(extractor)
 
     return 0
 
