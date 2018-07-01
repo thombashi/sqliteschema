bc5b39e38d570afe98a72880eb464b406cb3a55c
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-07-01 19:45:28 +0900

    Add a test file

---
+++ test/test_schema.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import json
+from textwrap import dedent
+
+import pytest
+from sqliteschema._schema import SQLiteTableSchema
+
+
+class Test_SQLiteTableSchema_constructor(object):
+
+    def test_exception(self):
+        with pytest.raises(ValueError):
+            SQLiteTableSchema("not_exist_table", {})
