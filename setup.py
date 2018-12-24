2564ad7f9cd295e5e364522456ceed145c3cbff4
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-12-24 16:26:23 +0900

    Update requirements

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 SETUPTOOLS_REQUIRES = ["setuptools>=38.3.0"]
 PYTEST_RUNNER_REQUIRES = ["pytest-runner"] if need_pytest() else []
 
-dumps_requires = ["pytablewriter>=0.36.0"]
+dumps_requires = ["pytablewriter>=0.36.1"]
 tests_requires = frozenset(tests_requires + dumps_requires)
 
 setuptools.setup(
