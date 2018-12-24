7196e592b4c77addd4957c8c92144a97dbb57c88
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-12-24 16:25:50 +0900

    Modify Python package installation for Travis CI

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,9 +6,7 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
         brew upgrade python
     fi
 
-    pip3 install setuptools --upgrade
-    pip3 install .[dumps,test]
+    pip3 install setuptools tox --upgrade
 else
-    pip install setuptools --upgrade
-    pip install .[dumps,test]
+    pip install setuptools tox --upgrade
 fi
