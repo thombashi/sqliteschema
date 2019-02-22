d86cfb740f4119049a7ac293037f9cba12a3516b
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-02-23 06:42:22 +0900

    Change a constant type from list to tuple

--- sqliteschema/_const.py
+++ sqliteschema/_const.py
@@ -8,14 +8,14 @@
 MAX_VERBOSITY_LEVEL = 100
 
 # https://www.sqlite.org/fileformat2.html
-SQLITE_SYSTEM_TABLES = [
+SQLITE_SYSTEM_TABLES = (
     "sqlite_master",
     "sqlite_sequence",
     "sqlite_stat1",
     "sqlite_stat2",
     "sqlite_stat3",
     "sqlite_stat4",
-]
+)
 
 SQLITE_SYSTEM_TABLE_LIST = SQLITE_SYSTEM_TABLES  # deprecated
 
