63f53ee3148b9dec54df2c5bb14d8a16824f2417
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-10-22 01:09:35 +0900

    Add __all__ to modules

--- sqliteschema/__init__.py
+++ sqliteschema/__init__.py
@@ -5,5 +5,21 @@
 from .__version__ import __author__, __copyright__, __email__, __license__, __version__
 from ._const import SQLITE_SYSTEM_TABLES, SchemaHeader
 from ._error import DataNotFoundError
-from ._extractor import SQLiteSchemaExtractor
+from ._extractor import SQLiteSchemaExtractor, SQLiteTableSchema
 from ._logger import set_log_level, set_logger
+
+
+__all__ = (
+    "__author__",
+    "__copyright__",
+    "__email__",
+    "__license__",
+    "__version__",
+    "DataNotFoundError",
+    "SchemaHeader",
+    "SQLiteSchemaExtractor",
+    "SQLiteTableSchema",
+    "SQLITE_SYSTEM_TABLES",
+    "set_log_level",
+    "set_logger",
+)
