600cb7cdf8a2dc1c5fe46df2f83964fe32423232
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-10-21 21:28:31 +0900

    Drop support for python 3.6

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setuptools.setup(
         "Source": REPOSITORY_URL,
         "Tracker": f"{REPOSITORY_URL:s}/issues",
     },
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=install_requires,
     extras_require={
         "cli": dumps_requires + logging_requires,
@@ -77,7 +77,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
